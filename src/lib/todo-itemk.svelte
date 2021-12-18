009bbc98ccd7ac2066cb429a2387c44bf6f82c8c
Pierluigi <ildragoneblu@libero.it>
2021-12-18 10:16:17 +0000

    develop html structure

---
+++ src/lib/todo-itemk.svelte
@@ -0,0 +1,16 @@
+<div>
+    <form method="" action="">
+        <input type="hidden" name="done" >
+        <button aria-label="Mark done / not done">Done/Not Done</button>
+    </form>
+
+    <form method="" action="">
+        <input type="hidden" name="done" >
+        <button aria-label="Save to do">Save</button>
+    </form>
+
+    <form method="" action="">
+        <input type="hidden" name="done" >
+        <button aria-label="Mark done / not done">Done/Not Done</button>
+    </form>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import TodoItem from "$lib/todo-itemk.svelte"
+
+    const title = "Todo"
+</script>
+
+<svelt:head><title>{title}</title></svelt:head>
+<h1>{title}</h1>
+<form method="" action="">
+    <input type="text" name="text" placeholder="+ add a tap bla  bla">
+</form>
+<TodoItem/>
+<TodoItem/>
+<TodoItem/>
+
